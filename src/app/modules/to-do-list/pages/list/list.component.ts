22c1e6f000590a3b245f4981b000d944fc45776a
Lucas Paduam <lucasepaduam@gmail.com>
2024-04-15 19:13:09 -0400

    CSS Page List

--- src/app/modules/to-do-list/pages/list/list.component.ts
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -26,4 +26,9 @@ export class ListComponent {
     return this.#setListItems.set(this.#parseItems());
   }
 
+  public deleteAllItems(){
+    localStorage.removeItem('@mylist');
+    return this.#setListItems.set(this.#parseItems());
+  }
+
 }
